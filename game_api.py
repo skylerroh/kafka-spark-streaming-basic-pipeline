8bd28bc05f23dc60140ba912a2edf6f7bc5c1f8d
skylerroh <skyroh@berkeley.edu>
2020-04-01 04:46:41 +0000

    flask with redis for user state

--- game_api.py
+++ game_api.py
@@ -19,23 +19,61 @@ def get_user_state(user):
 
 
 def get_user_inventory(user):
+    user = user
     user_info = get_user_state(user)
     return user_info['inventory']
 
-def get_user_guild():
+
+def get_user_guild(user):
+    user = user
     user_info = get_user_state(user)
     return user_info['guild']
+
+
+def update_user_state(user, attribute):
+    '''
+    user_id: user id string
+    attribute: either guild or inventory dict to update
+    '''
+    user_state = get_user_state(user)
+    user_state.update(attribute)
+    r.set(user, json.dumps(user_state).encode())
+
 
 def log_to_kafka(topic, event):
     user = get_user()
     event.update(request.headers)
     event.update({'username': user})
     producer.send(topic, json.dumps(event).encode())
-
+    
+    
+def change_inventory(user, item_category, quantity, item_type='basic'):
+    if quantity == 0:
+        return
+    
+    inventory = get_user_inventory(user)
+    category = inventory.get(item_category, {})
+    cur_quantity = category.get(item_type, 0)
+    if quantity < 0:
+        status = 'Failed: Do not have that item to sell' if cur_quantity == 0 else 'Success'
+    else:
+        status = 'Success'
+    
+    category[item_type] = max(cur_quantity + quantity, 0)
+    inventory[item_category] = category
+    update_user_state(user, {'inventory': inventory})
+    inventory_event = {'event_type': '{}_transaction'.format(item_category),
+                       'transaction_type': 'purchase' if quantity > 0 else 'sell',
+                       'item_name': item_type,
+                       'status': status}
+    log_to_kafka('events', inventory_event)
+    return inventory_event
+    
 
 @app.route("/")
 def default_response():
-    default_event = {'event_type': 'default'}
+#     default_event = {'event_type': 'default'}
+    default_event = {'keys': r.keys('*'), 'user': r.get('user1')}
     log_to_kafka('events', default_event)
     return "This is the default response!\n"
 
@@ -43,44 +81,53 @@ def default_response():
 @app.route("/login")
 def set_user():
     user = get_user()
+    if not r.get(user):
+        r.set(user, json.dumps({"guild": None, "inventory": {}}).encode())
     session['username'] = user
     return "Current User: {}\n".format(session.get('username'))
 
 
-@app.route("/logout")
-def remove_user():
-    session.pop('username', None)
-    return "Logged Out\n"
-
-
 @app.route("/purchase_a_sword")
-def purchase_basic_sword():
-    return purchase_a_sword('basic_sword')
-
-
-@app.route("/purchase_a_sword/<sword_type>")
-def purchase_a_sword(sword_type):
-    purchase_sword_event = {'event_type': 'purchase_sword',
-                            'sword_type': sword_type}
-    log_to_kafka('events', purchase_sword_event)
+def purchase_a_sword():
+    sword_type = request.args.get('sword_type', 'basic_sword')
+    quantity = request.args.get('quantity', 1)
+    user = get_user()
+    event = change_inventory(user=user, item_category='sword', item_type=sword_type, quantity=quantity)
     return "Sword Purchased: {}!\n".format(sword_type)
 
 
 @app.route("/purchase_a_knife")
 def purchase_a_knife():
-    purchase_knife_event = {'event_type': 'purchase_knife',
-                            'description': 'very sharp knife'}
-    log_to_kafka('events', purchase_knife_event)
+    user = get_user()
+    knife_type = 'very_sharp_knife'
+    event = change_inventory(user=user, item_category='knife', item_type=knife_type, quantity=1)
     return "Knife Purchased!\n"
+
+
+@app.route("/sell_a_sword")
+def sell_a_sword():
+    sword_type = request.args.get('sword_type', 'basic_sword')
+    quantity = request.args.get('quantity', 1)
+    user = get_user()
+    event = change_inventory(user=user, item_category='sword', item_type=sword_type, quantity=-quantity)
+    return "Sword Sold: {}!\n".format(sword_type)
+
+
+@app.route("/sell_a_knife")
+def sell_a_knife():
+    user = get_user()
+    event = change_inventory(user=user, item_category='knife', item_type='very sharp knife', quantity=-1)
+    return "Knife Sold!\n"
 
 
 @app.route("/join_guild/<guild_name>")
 def join_guild(guild_name):
     user = get_user()
     guild = get_user_guild(user)
-    if user_info.get('Guild'):
+    if guild:
         status = "Failed: Already part of a guild"
     else:
+        update_user_state(user, {'Guild': guild_name})
         status = "Success: Joined {}".format(guild_name)
     join_guild_event = {'event_type': 'join_guild',
                         'guild_name': guild_name,
@@ -90,19 +137,20 @@ def join_guild(guild_name):
 
 
 @app.route("/leave_guild")
-def leave_guild(guild_name):
+def leave_guild():
     user = get_user()
     guild = get_user_guild(user)
     if guild:
+        update_user_state(user, {"guild": None})
         status = "Success: Left guild {}".format(guild)
     else: 
         status = "Failed: Not currently part of a guild"
         
     leave_guild_event = {'event_type': 'left_guild',
-                         'guild_name': guild_name,
+                         'guild_name': guild,
                          'status': status}
     log_to_kafka('events', leave_guild_event)
-    return "Left Guild: {}!\n".format(guild_name)
+    return "Left Guild: {}!\n".format(guild)
 
 
 if __name__ == '__main__':
--- test_events.sh
+++ test_events.sh
@@ -1,11 +1,47 @@
 # use apache bench to runsome requests
+# create the users
 ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/login 
 ab -n 1 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/login
-ab -n 10 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/
-ab -n 10 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword
-ab -n 10 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/
+ab -n 1 -H "Host: user3.googlefi.com" -C 'UserCookie=user3' http://localhost:5000/login
+ab -n 1 -H "Host: user4.googlefi.com" -C 'UserCookie=user4' http://localhost:5000/login
+ab -n 1 -H "Host: user5.comcast.com" -C 'UserCookie=user5' http://localhost:5000/login
+
+# buy/sell some stuff
+ab -n 20 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword
+ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/
+ab -n 15 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword?sword_type=cool_sword
+ab -n 10 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword?sword_type=good_sword
+ab -n 12 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/sell_a_sword?sword_type=basic_sword
+ab -n 6 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/sell_a_sword?sword_type=good_sword
+ab -n 3 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword?sword_type=better_sword
+ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword?sword_type=best_sword
+
+# wait 20 sec
+sleep 20
+
 ab -n 10 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/purchase_a_sword
-ab -n 10 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_sword/cool_sword
+ab -n 9 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/sell_a_sword?sword_type=basic_sword
+ab -n 1 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/purchase_a_sword?sword_type=best_sword
+
+ab -n 30 -H "Host: user3.googlefi.com" -C 'UserCookie=user3' http://localhost:5000/purchase_a_sword
+ab -n 3 -H "Host: user3.googlefi.com" -C 'UserCookie=user3' http://localhost:5000/purchase_a_sword?sword_type=better_sword
+
+ab -n 2 -H "Host: user4.googlefi.com" -C 'UserCookie=user4' http://localhost:5000/purchase_a_sword
+
+ab -n 3 -H "Host: user5.comcast.com" -C 'UserCookie=user5' http://localhost:5000/purchase_a_sword
+
+ab -n 3 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/purchase_a_knife
+ab -n 1 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/purchase_a_knife
+ab -n 2 -H "Host: user3.googlefi.com" -C 'UserCookie=user3' http://localhost:5000/purchase_a_knife
+ab -n 1 -H "Host: user4.googlefi.com" -C 'UserCookie=user4' http://localhost:5000/purchase_a_knife
+
+# join some guilds
+ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/join_guild/the_brave
+ab -n 1 -H "Host: user2.att.com" -C 'UserCookie=user2' http://localhost:5000/join_guild/the_brave
+ab -n 2 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/leave_guild
+ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/join_guild/the_brave
+ab -n 1 -H "Host: user1.comcast.com" -C 'UserCookie=user1' http://localhost:5000/join_guild/the_honorable
+ab -n 1 -H "Host: user3.googlefi.com" -C 'UserCookie=user3' http://localhost:5000/join_guild/the_brave
 
 # read from kafka and check number of events
 kafkacat -C -b kafka:29092 -t events -o beginning -e | wc -l
